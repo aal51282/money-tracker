c01bff9ff5d0701438ec512b11723b21211c9d92
Angel <aal51282@uga.edu>
2024-10-12 14:54:54 -0400

    Completed frontend.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <main>
       <h1>$400<span>.00</span></h1>
       <form>
-        <div className='basic'>
-          <input type="text" placeholder={'+200 new samsung tv'} />
+        <div className="basic">
+          <input type="text" placeholder={"+200 new samsung tv"} />
           <input type="datetime-local" />
         </div>
-        <div className='description'>
-          <input type="text" placeholder={'description'} />
+        <div className="description">
+          <input type="text" placeholder={"description"} />
         </div>
         <button type = "submit">Add new transaction</button>
       </form>
-      
+      <div className="transactions">
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Samsung TV</div>
+            <div className="description">It was time for a new TV</div>
+          </div>
+          <div className="right">
+            <div className="price red">-$500</div>
+            <div className="datetime">2024-01-01 15:45</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Gig job new website</div>
+            <div className="description">It was time for a new TV</div>
+          </div>
+          <div className="right">
+            <div className="price green">+$400</div>
+            <div className="datetime">2024-01-01 15:45</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">iPhone 15</div>
+            <div className="description">It was time for a new TV</div>
+          </div>
+          <div className="right">
+            <div className="price red">-$900</div>
+            <div className="datetime">2024-01-01 15:45</div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
