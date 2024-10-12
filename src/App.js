b6988e9672d5555f499825ef7439e859d6ce6f0c
Angel <aal51282@uga.edu>
2024-10-12 14:06:16 -0400

    Initial Commit.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>
+        <div className='basic'>
+          <input type="text" placeholder={'+200 new samsung tv'} />
+          <input type="datetime-local" />
+        </div>
+        <div className='description'>
+          <input type="text" placeholder={'description'} />
+        </div>
+        <button type = "submit">Add new transaction</button>
+      </form>
+      
+    </main>
+  );
+}
+
+export default App;
