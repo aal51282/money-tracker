d52fe27b3697b11e0f624e0d957a499d0c49286a
Angel <aal51282@uga.edu>
2024-10-12 17:07:32 -0400

    Ran into runtime error.

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const Transaction = require('./models/Transaction.js');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/test', (req, res) => {
+    res.json({ body: 'test ok' });
+});
+
+app.post('/api/transaction', async (req, res) => {
+    await mongoose.connect(process.env.MONGO_URL); // Connect to Database
+    const { name, description, datetime } = req.body;
+    const transaction = await Transaction.create({name, description, datetime});
+    res.json(transaction);
+});
+
+app.listen(4040);
--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [datetime, setDatetime] = useState("");
+  const [description, setDescription] = useState("");
+
+  function addNewTransaction(e) {
+    e.preventDefault();
+    const url = process.env.REACT_APP_API_URL + '/transaction';
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name, description, datetime }),
+    }).then(response => {
+      response.json().then(json => {
+        console.log('result:', json);
+      });
+    });
+  }
+
   return (
     <main>
       <h1>$400<span>.00</span></h1>
-      <form>
+      <form onSubmit={addNewTransaction}>
         <div className="basic">
-          <input type="text" placeholder={"+200 new samsung tv"} />
-          <input type="datetime-local" />
+          <input type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder={"+200 new samsung tv"} />
+          <input type="datetime-local"
+            value={datetime}
+            onChange={(e) => setDatetime(e.target.value)} />
         </div>
         <div className="description">
-          <input type="text" placeholder={"description"} />
+          <input type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder={"description"} />
         </div>
-        <button type = "submit">Add new transaction</button>
+        <button type="submit">Add new transaction</button>
       </form>
       <div className="transactions">
         <div className="transaction">
