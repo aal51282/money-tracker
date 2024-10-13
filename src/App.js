74f11aa4291b1b0ae5b9136d367413fe2b199faf
Angel <aal51282@uga.edu>
2024-10-13 01:58:56 -0400

    Fully working app.

--- src/App.js
+++ src/App.js
@@ -40,9 +40,17 @@ function App() {
     });
   }
 
+  let balance = 0;
+  for (const transaction of transactions) {
+    balance += transaction.price;
+  }
+
+  balance = balance.toFixed(2);
+  const fraction = balance.split('.')[1];
+  balance = balance.split('.')[0];
   return (
     <main>
-      <h1>$400<span>.00</span></h1>
+      <h1>${balance}<span>.{fraction}</span></h1>
       <form onSubmit={addNewTransaction}>
         <div className="basic">
           <input type="text"
