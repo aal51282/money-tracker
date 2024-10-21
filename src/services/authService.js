8217370b9dc5db20c3794216a49585b296d12ebc
Angel <aal51282@uga.edu>
2024-10-21 00:27:38 -0400

    Fixed register response for vercel.

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const register = async (username, password) => {
-    const response = await axios.post(`${API_URL}/register`, { username, password });
+    const response = await axios.post('https://money-tracker-backend-oc8t.onrender.com/api/register', {
+        username: username,
+        password: password
+    });
     return response.data;
 };
 
